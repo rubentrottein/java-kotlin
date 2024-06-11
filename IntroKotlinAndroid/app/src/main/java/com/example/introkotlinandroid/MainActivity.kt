eb47db8f69811510012f29263c45adcff1cb15bd
rubentrottein <38758157+rubentrottein@users.noreply.github.com>
2024-06-11 16:59:32 +0200

    Ajout du projet d'Intro

---
+++ IntroKotlinAndroid/app/src/main/java/com/example/introkotlinandroid/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.introkotlinandroid
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.text.Layout
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //ids textView button
+        val myButton = findViewById<Button>(R.id.button)
+
+        val messageTextView = findViewById<TextView>(R.id.textView)
+        val messageTextView2 = findViewById<TextView>(R.id.textView2)
+        val messageTextView3 = findViewById<TextView>(R.id.textView3)
+        val messageTextView4 = findViewById<TextView>(R.id.textView4)
+
+        myButton.setOnClickListener(View.OnClickListener {
+                val saveOne = messageTextView.text;
+                val save = messageTextView2.text;
+                messageTextView.setText(save)
+                messageTextView2.setText(saveOne)
+        })
+        println(messageTextView);
+        println(messageTextView2);
+
+        val roll= findViewById<ImageView>(R.id.imageView)
+        val dice= findViewById<ImageView>(R.id.imageView2)
+
+        roll.setOnClickListener(View.OnClickListener {
+            val number = Random.nextInt(from=0 ,until = 6);
+            val images = arrayOf(
+                R.drawable.one,
+                R.drawable.two,
+                R.drawable.three,
+                R.drawable.four,
+                R.drawable.five,
+                R.drawable.six
+            );
+            try{
+                dice.setImageResource(images[number]);
+                dice.setImageResource(R.drawable.six);
+                messageTextView4.setText("Image par OpenClipart-Vectors de Pixabay")
+            } catch (e : Throwable ){
+                dice.setImageResource(R.drawable.three);
+                messageTextView3.setText(e.toString());
+                messageTextView3.setText(images[number]);
+            }
+        })
+
+        findViewById<ImageView>(R.id.next).setOnClickListener(View.OnClickListener {
+            val intentNextActivity : Intent = Intent(this, NextActivity::class.java)
+            startActivity(intentNextActivity);
+        })
+    }
+}
---
+++ IntroKotlinAndroid/app/src/main/java/com/example/introkotlinandroid/NextActivity.kt
@@ -0,0 +1,30 @@
+package com.example.introkotlinandroid
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class NextActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_next)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<ImageView>(R.id.previous).setOnClickListener(View.OnClickListener {
+            val intentNextActivity : Intent = Intent(this, MainActivity::class.java)
+            startActivity(intentNextActivity);
+        })
+    }
+
+
+}
